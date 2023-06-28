35a9df384221733ee36da30a6429cf716ecf5111
Arik Rahman <40479733+ArikRahman@users.noreply.github.com>
2023-06-28 15:10:39 -0500

    Introduced footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,10 +11,13 @@
 
 <FooterComponent></FooterComponent>
 
-
+<WaveFooter>
+    
+</WaveFooter>
 
 <script>
     import HeaderComponent from "../components/HeaderComponent.svelte";
     import TopNav from "../components/TopNav.svelte";
     import FooterComponent from "../components/FooterComponent.svelte";
+    import WaveFooter from "../components/WaveFooter.svelte";
 </script>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -12,13 +12,13 @@
 
 
 <FooterComponent></FooterComponent>
-
+<WaveFooter></WaveFooter>
 <script>
     import HeaderComponent from "../../components/HeaderComponent.svelte";
     import TopNav from "../../components/TopNav.svelte";
     import Headshot from "../../components/Headshot.svelte";
     import FooterComponent from "../../components/FooterComponent.svelte";
-
+    import WaveFooter from "../../components/WaveFooter.svelte";
 
 
 </script>
