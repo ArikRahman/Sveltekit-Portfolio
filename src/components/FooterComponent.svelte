eab67b2629977c0a5db45ada1408da0866c073ac
Arik Rahman <40479733+ArikRahman@users.noreply.github.com>
2023-06-28 14:34:04 -0500

    Update FooterComponent.svelte

--- src/components/FooterComponent.svelte
+++ src/components/FooterComponent.svelte
@@ -1,7 +1,7 @@
 <footer>
     <img src="https://pbs.twimg.com/profile_images/411953318076755968/qrMmLUjA.jpeg" width = 5%>
-    <p>Author: Hege Refsnes</p>
-    <!-- <p><a href="mailto:hege@example.com">cilanclos@tamu.edu</a></p> -->
+    <!-- <p>Author: Arik Rahman</p> -->
+    <!-- <p><a href="mailto:arik@tamu.com">arik@tamu.edu</a></p> -->
 </footer>     
 
 
