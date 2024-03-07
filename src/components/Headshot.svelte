0e0468db62d885de77fede34900c8c0f082e9aef
Arik Rahman <40479733+ArikRahman@users.noreply.github.com>
2024-03-06 20:50:08 -0600

    Update Headshot.svelte

--- src/components/Headshot.svelte
+++ src/components/Headshot.svelte
@@ -23,5 +23,5 @@
 </style>
 
 <script>
-    let url = "https://media.licdn.com/dms/image/D5603AQGQCffHl8shGQ/profile-displayphoto-shrink_800_800/0/1683310419377?e=1693440000&v=beta&t=07_hwdI2xn2--0Mgx7zV2ininJ2YYwRK3cEOwH4ODgg" 
+    let url = "https://media.licdn.com/dms/image/D5603AQEtPAFBt42ajA/profile-displayphoto-shrink_200_200/0/1708056541133?e=1715212800&v=beta&t=zEdGcaawcUfdj1ivmW3MKeofFsSMdSL5zUDXsDsAqpw" 
 </script>
